ff3c65e98ada210f17b2578c18057b614d52b300
Shogo Ohta <athos0220@gmail.com>
2020-01-30 21:26:03 +0900

    first commit

---
+++ src/pogonos/core.clj
@@ -0,0 +1,6 @@
+(ns pogonos.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/pogonos/core_test.clj
@@ -0,0 +1,7 @@
+(ns pogonos.core-test
+  (:require [clojure.test :refer :all]
+            [pogonos.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
