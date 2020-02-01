57ea1be8f54120c56a91dff1021e194c95cad5ac
Shogo Ohta <athos0220@gmail.com>
2020-02-01 21:04:13 +0900

    Add ClojureScript as dep

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/athos/pogonos"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:provided
+             {:dependencies [[org.clojure/clojure "1.10.1"]
+                             [org.clojure/clojurescript "1.10.597"]]}}
   :repl-options {:init-ns pogonos.core})
