56987d84441f47f3cc64d977a5e9ff1ebec37124
Shogo Ohta <athos0220@gmail.com>
2020-06-18 22:01:25 +0900

    Update library description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject pogonos "0.1.0-SNAPSHOT"
-  :description "Another Clojure implementation of the Mustache templating language"
+  :description "Another Clojure(Script) implementation of the Mustache templating language"
   :url "https://github.com/athos/pogonos"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
