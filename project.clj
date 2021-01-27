93715663ae3dad637151917d82675f4458e04f90
Shogo Ohta <athos0220@gmail.com>
2021-01-27 22:25:35 +0900

    Update Clojure

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :profiles {:provided
-             {:dependencies [[org.clojure/clojure "1.10.1"]
+             {:dependencies [[org.clojure/clojure "1.10.2"]
                              [org.clojure/clojurescript "1.10.597"]]}
              :dev
              ;; these are necessary only for tests, so ideally they should go
