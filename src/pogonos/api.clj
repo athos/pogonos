285eac2049564483f67f2009c96b3aaca8519fe6
Shogo Ohta <athos0220@gmail.com>
2022-01-27 23:56:18 +0900

    Add another API fn: check

--- src/pogonos/api.clj
+++ src/pogonos/api.clj
@@ -3,7 +3,8 @@
             [clojure.java.io :as io]
             [pogonos.core :as pg]
             [pogonos.output :as out]
-            [pogonos.reader :as reader])
+            [pogonos.reader :as reader]
+            [pogonos.error :as error])
   (:import [java.io PushbackReader]))
 
 (defn render [{:keys [string file resource data data-file output] :as opts}]
@@ -22,3 +23,15 @@
           file (pg/render-file (str file) data opts')
           resource (pg/render-resource (str resource) data opts')
           :else (pg/render-input (reader/->reader *in*) data opts'))))
+
+(defn check [{:keys [string file resource] :as opts}]
+  (try
+    (cond string (pg/check-string string opts)
+          file (pg/check-file (str file) opts)
+          resource (pg/check-resource (str resource) opts)
+          :else (pg/check-input (reader/->reader *in*) opts))
+    (catch Exception e
+      (if (::error/type (ex-data e))
+        (throw (ex-info (str "Template parsing failed: " (ex-message e))
+                        (ex-data e)))
+        (throw e)))))
