ab4980dd454f3f7aca2f46b46e5d4df3a511ef6c
Shogo Ohta <athos0220@gmail.com>
2022-01-27 20:50:24 +0900

    Add :data-file option

--- src/pogonos/api.clj
+++ src/pogonos/api.clj
@@ -1,15 +1,23 @@
 (ns pogonos.api
-  (:require [pogonos.core :as pg]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [pogonos.core :as pg]
             [pogonos.output :as out]
-            [pogonos.reader :as reader]))
+            [pogonos.reader :as reader])
+  (:import [java.io PushbackReader]))
 
-(defn render [{:keys [string file resource data output] :as opts}]
-  (let [out (if output
+(defn render [{:keys [string file resource data data-file output] :as opts}]
+  (let [data (or (when data-file
+                   (with-open [r (-> (io/reader (str data-file))
+                                     PushbackReader.)]
+                     (edn/read r)))
+                 data)
+        out (if output
               (out/to-file (str output))
               (out/to-stdout))
         opts' (-> opts
                   (assoc :output out)
-                  (dissoc :string :file :resource :data))]
+                  (dissoc :string :file :resource :data :data-file))]
     (cond string (pg/render-string string data opts')
           file (pg/render-file (str file) data opts')
           resource (pg/render-resource (str resource) data opts')
