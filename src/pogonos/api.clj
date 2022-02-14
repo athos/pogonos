c4b17cad0179ce9654d2ae706d6b4492398ab619
Shogo Ohta <athos0220@gmail.com>
2022-02-14 23:34:06 +0900

    Support bulk check for specified dir(s)

--- src/pogonos/api.clj
+++ src/pogonos/api.clj
@@ -1,11 +1,13 @@
 (ns pogonos.api
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [clojure.string :as str]
             [pogonos.core :as pg]
+            [pogonos.error :as error]
             [pogonos.output :as out]
-            [pogonos.reader :as reader]
-            [pogonos.error :as error])
-  (:import [java.io PushbackReader]))
+            [pogonos.reader :as reader])
+  (:import [java.io File PushbackReader]
+           [java.util.regex Pattern]))
 
 (defn render [{:keys [string file resource data data-file output] :as opts}]
   (let [data (or (when data-file
@@ -24,10 +26,21 @@
           resource (pg/render-resource (str resource) data opts')
           :else (pg/render-input (reader/->reader *in*) data opts'))))
 
-(defn check [{:keys [string file resource] :as opts}]
+(def ^:private ^:const path-separator
+  (re-pattern (Pattern/quote (System/getProperty "path.separator"))))
+
+(defn- check-files [dir opts]
+  (let [dirs (str/split dir path-separator)]
+    (doseq [dir dirs
+            ^File file (file-seq (io/file dir))
+            :when (.isFile file)]
+      (pg/check-file file opts))))
+
+(defn check [{:keys [string file dir resource] :as opts}]
   (try
     (cond string (pg/check-string string opts)
           file (pg/check-file (str file) opts)
+          dir (check-files (str dir) opts)
           resource (pg/check-resource (str resource) opts)
           :else (pg/check-input (reader/->reader *in*) opts))
     (catch Exception e
