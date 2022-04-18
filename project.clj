84ea4f46b10070f06e664e7bc2c2b242fbcb17a6
Shogo Ohta <athos0220@gmail.com>
2022-04-18 22:49:15 +0900

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pogonos "0.2.0-SNAPSHOT"
+(defproject pogonos "0.2.0"
   :description "Another Clojure(Script) implementation of the Mustache templating language"
   :url "https://github.com/athos/pogonos"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
